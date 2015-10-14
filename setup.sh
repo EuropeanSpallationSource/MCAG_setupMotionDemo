747d8e133c0b8c3cbafe7c980a20f06317ba3020
Torsten Bögershausen <torsten.bogershausen@esss.se>
2015-10-14 07:28:16 +0200

    Add READBACK

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ if ! test -d "$DIR"; then
   git clone https://github.com/EuropeanSpallationSource/$DIR.git &&
   (
     cd $DIR &&
-    git checkout 11eab43836a9376fd0
+    git checkout  0a3597b9f4ce113fa8cd4add853ed
   )
 fi
