10031858448cd7fca5268cf89371201c3400e1f8
Michael Wedel <torsten.bogershausen@esss.se>
2016-05-09 11:46:01 +0200

    setup: Install IcePAP

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-DIR=MCAG_Base_Project
+DIRT=MCAG_Base_Project
+DIRI=m-epics-IcePAP
 
 ./install-epics.sh "$@" &&
-if ! test -d "$DIR"; then	
-  git clone https://github.com/EuropeanSpallationSource/$DIR.git &&
+if ! test -d "$DIRT"; then	
+  git clone https://github.com/EuropeanSpallationSource/$TDIR.git &&
   (
     cd $DIR &&
       git checkout f14022e871acd3bf837e155fe8b7f0ed0
+  )
+fi
+
+if ! test -d "$DIRI"; then	
+  git clone https://github.com/EuropeanSpallationSource/$TDIR.git &&
+  (
+    cd $DIR &&
+      git checkout 160429_Real_IcePAP_axis_2
   )
 fi
