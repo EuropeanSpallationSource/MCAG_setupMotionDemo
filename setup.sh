28ef562b57f514eaba949fea900e84c717723dc1
Torsten Bögershausen <torsten.bogershausen@esss.se>
2016-05-12 09:58:53 +0200

    Fix wrong DIR macros

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ DIRI=m-epics-IcePAP
 
 ./install-epics.sh "$@" &&
 if ! test -d "$DIRT"; then	
-  git clone https://github.com/EuropeanSpallationSource/$TDIR.git &&
+  git clone https://github.com/EuropeanSpallationSource/$DIRT.git &&
   (
-    cd $DIR &&
+    cd $DIRT &&
       git checkout f14022e871acd3bf837e155fe8b7f0ed0
   )
 fi
 
 if ! test -d "$DIRI"; then	
-  git clone https://github.com/EuropeanSpallationSource/$TDIR.git &&
+  git clone https://github.com/EuropeanSpallationSource/$DIRI.git &&
   (
-    cd $DIR &&
+    cd $DIRI &&
       git checkout 160429_Real_IcePAP_axis_2
   )
 fi
